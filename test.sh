5e639b21d776b653522a1aafa2b54ecbfbbf78d8
rabbit2123 <125840784+rabbit2123@users.noreply.github.com>
2023-04-18 09:03:17 +0800

    optimize test

--- test.sh
+++ test.sh
@@ -7,7 +7,9 @@ RET=0
 
 test_cloudflare() {
     local url=""
-
+    local tmp_file=$(mktemp)
+    cat $CLOUDFLARE_FILE |egrep -v '^ *$|^ *#' > $tmp_file
+    
     while IFS='' read -r line; do
         echo "test cloudflare: $line"
         url="https://www.cloudflare.com/ --connect-to ::${line}"
@@ -15,12 +17,16 @@ test_cloudflare() {
         if [ $? -ne 0 ]; then
             RET=1
         fi
-    done < "$CLOUDFLARE_FILE"
+    done < "$tmp_file"
+    
+    rm $tmp_file
 }
 
 test_google() {
     local url=""
-
+    local tmp_file=$(mktemp)
+    cat $GOOGLE_FILE |egrep -v '^ *$|^ *#' > $tmp_file
+    
     while IFS='' read -r line; do
         echo "test google: $line"
         url="https://www.google.com/ --connect-to ::${line}"
@@ -28,7 +34,9 @@ test_google() {
         if [ $? -ne 0 ]; then
             RET=1
         fi
-    done < "$GOOGLE_FILE"
+    done < "$tmp_file"
+    
+    rm $tmp_file
 }
 
 
