fbba5f30492160afca954ee99be199833673f1c1
rabbit2123 <125840784+rabbit2123@users.noreply.github.com>
2023-04-18 13:37:33 +0800

    not test google

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
   
 CLOUDFLARE_FILE="cloudflare.txt"
-GOOGLE_FILE="google.txt"
 RET=0
 
 
@@ -22,24 +21,6 @@ test_cloudflare() {
     rm $tmp_file
 }
 
-test_google() {
-    local url=""
-    local tmp_file=$(mktemp)
-    cat $GOOGLE_FILE |egrep -v '^ *$|^ *#' > $tmp_file
-    
-    while IFS='' read -r line; do
-        echo "test google: $line"
-        url="https://www.google.com/ --connect-to ::${line}"
-        curl --max-time 3 -sS -I $url >/dev/null
-        if [ $? -ne 0 ]; then
-            RET=1
-        fi
-    done < "$tmp_file"
-    
-    rm $tmp_file
-}
-
 
 test_cloudflare
-test_google
 exit $RET
